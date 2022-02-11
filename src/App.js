1bfbaa5963de5aca8535fd9efc836fe91d029ce0
Park Chenil <s10e01s@naver.com>
2022-02-12 00:40:58 +0900

    state 사용법, ES6문법, 웹앱처럼 동작하기 위한 저장소state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let posts = { color: "blue", fontSize: "30px" };
+  let [글제목, 글제목변경] = useState([
+    "남자 코트 추천",
+    "강남 우동맛집",
+    "리엑트공부",
+  ]);
+  let posts = "강남 고기 맛집";
 
   return (
     <div className="App">
       <div className="black-nav">
-        <div style={posts}>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <h4> 블로그내 글 </h4>
+      <div className="list">
+        <h3> {글제목[0]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[1]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> {글제목[2]} </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
